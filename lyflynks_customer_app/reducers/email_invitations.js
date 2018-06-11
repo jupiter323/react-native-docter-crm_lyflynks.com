600c5a8306a23211bc47c2f46798a78e95765a34
MOTIFWORKS\anubhav.seal <anubhav.seal@motifworks.com>
2018-06-11 13:03:14 +0530

    Boilerplate for email_invite

--- lyflynks_customer_app/reducers/email_invitations.js
+++ lyflynks_customer_app/reducers/email_invitations.js
@@ -1,39 +1,22 @@
 import {
   ADD_EMAIL,
   EDIT_EMAIL,
-  DELETE_EMAIL
+  DELETE_EMAIL,
+  SENDING_EMAIL_INVITATIONS,
+  EMAIL_INIVITATIONS_SUCCESS,
+  EMAIL_INIVITATIONS_FAILURE
 } from "../actions/email_invitations";
 
 const INITITAL_STATE = {
   count: 1,
   invitations: {
     id1: { id: "id1", email: "" }
-  }
+  },
+  sentSuccessfully: false
 };
 
 export default (state = INITITAL_STATE, action) => {
   switch (action.type) {
-    case "ADD_EMAIL":
-      const nextCount = state.count + 1;
-      const newInvitations = {
-        ...state.invitations,
-        [`id${nextCount}`]: { id: `id${nextCount}`, email: "" }
-      };
-      return { ...state, count: nextCount, invitations: newInvitations };
-    case "EDIT_EMAIL":
-      const inviteToEdit = state.invitations[action.payload.id];
-      const updatedInvite = { ...inviteToEdit, email: action.payload.email };
-      const updatedInvitations = {
-        ...state.invitations,
-        [action.payload.id]: updatedInvite
-      };
-      return { ...state, invitations: updatedInvitations };
-    case "DELETE_EMAIL":
-      const {
-        [action.payload.id]: email,
-        ...nextInvitations
-      } = state.invitations;
-      return { ...state, invitations: nextInvitations };
     default:
       return state;
   }
