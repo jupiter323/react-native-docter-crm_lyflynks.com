39afdbda43c16545f1cb9365984b5d578316b6cb
MOTIFWORKS\anubhav.seal <anubhav.seal@motifworks.com>
2018-06-14 00:30:20 +0530

    Implementation Of Signup complete form

--- lyflynks_customer_app/components/Signup Forms/SignupComplete/SignupComplete.js
+++ lyflynks_customer_app/components/Signup Forms/SignupComplete/SignupComplete.js
@@ -1,25 +1,35 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Input, Card, Button } from "../../UI";
+import { Button } from "react-native-elements";
+
+import { INSTRUCTIONS_FOR_SIGNUP_COMPLETE } from "./instructions";
 
 class SignupComplete extends React.Component {
   render() {
     const { instructions, renderInstructions } = this.props;
     return (
-      <View
-        contentContainerStyle={styles.scrollViewContainer}
-        showsVerticalScrollIndicator={false}
-      >
-        {renderInstructions(instructions)}
+      <View style={styles.instructionContainer}>
+        <Text style={styles.instructionStyles}>
+          {INSTRUCTIONS_FOR_SIGNUP_COMPLETE}
+        </Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  scrollViewContainer: {
-    backgroundColor: "white",
-    alignItems: "center"
+  instructionContainer: {
+    marginTop: 160,
+    padding: 10
+  },
+  instructionStyles: {
+    fontSize: 16,
+    color: "black",
+    textAlign: "center",
+    fontWeight: "bold"
+  },
+  continueButton: {
+    marginTop: 30
   }
 });
 
--- lyflynks_customer_app/components/Signup Forms/SignupComplete/instructions.js
+++ lyflynks_customer_app/components/Signup Forms/SignupComplete/instructions.js
@@ -1,31 +1,16 @@
-const INSTRUCTIONS_FOR_SIGNUP_COMPLETE = [
-  {
-    id: 1,
-    text: "Your Lyn Account request has been received."
-  },
-  {
-    id: 2,
-    text:
-      "Congratulations for taking the first step in changing how your family cared for loved ones"
-  },
-  {
-    id: 3,
-    text:
-      "One of our Member Care Coordinators will call you in the next week to complete the sign up process."
-  },
-  {
-    id: 4,
-    text: "Your Lyn Account request has been received."
-  },
-  {
-    id: 5,
-    text:
-      "Account Members wil be emailed new member set up instructinos once your account has been activated."
-  },
-  {
-    id: 6,
-    text: "If you have questions please conatct us supprot@lyflnks.com"
-  }
-];
+const INSTRUCTIONS_FOR_SIGNUP_COMPLETE =
+  "Your Lyn Account request has been received." +
+  "\n" +
+  "\n" +
+  "Congratulations for taking the first step in changing how your family cared for loved ones" +
+  "\n" +
+  "\n" +
+  "One of our Member Care Coordinators will call you in the next week to complete the sign up process." +
+  "\n" +
+  "\n" +
+  "Account Members wil be emailed new member set up instructinos once your account has been activated." +
+  "\n" +
+  "\n" +
+  "If you have questions please conatct us supprot@lyflnks.com";
 
 export { INSTRUCTIONS_FOR_SIGNUP_COMPLETE };
