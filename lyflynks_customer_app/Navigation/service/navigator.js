93582577cb072308f8f0f9bc97f6920faab75a5a
Satish Mavani <satish.mavani@motifworks.com>
2018-09-14 18:59:44 +0530

    using react-native-custom-components in place of react native as depricated

--- lyflynks_customer_app/Navigation/service/navigator.js
+++ lyflynks_customer_app/Navigation/service/navigator.js
@@ -1,5 +1,5 @@
-import { NavigationActions } from 'react-navigation';
-import type { NavigationParams, NavigationRoute } from 'react-navigation';
+import { NavigationActions } from 'react-native-custom-components'
+import type { NavigationParams, NavigationRoute } from 'react-native-custom-components';
 
 let _container; // eslint-disable-line
 
