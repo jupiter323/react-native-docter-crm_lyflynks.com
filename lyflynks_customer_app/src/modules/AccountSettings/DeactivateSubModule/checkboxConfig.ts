af8587ad8c3e40242039fb6f5b48bffd7c446f47
Isaac Osei <iosei53@gmail.com>
2018-12-09 22:45:06 +0700

    fix

---
+++ lyflynks_customer_app/src/modules/AccountSettings/DeactivateSubModule/checkboxConfig.ts
@@ -0,0 +1,33 @@
+export default 
+[
+  {
+    "id": "transitioning",   
+    "errorId": "fullNameErrorMessage",
+    "statusId":"nameStatus",
+    "title":"Transitioning to Assisted Living"
+  },
+  {
+    "id": "expensive",
+    "errorId": "emailErrorMessage",
+    "statusId":"emailStatus",
+    "title":"Too Expensive"
+  },
+  {
+    "id": "elderpass",
+    "errorId": "primaryPhoneNumberErrorMessage",
+    "statusId":"primaryPhoneStatus",
+    "title":"Elder Passed Away"
+  },
+  {
+    "id": "eldermoving",  
+    "errorId": "secondaryPhoneNumberErrorMessage",
+    "statusId":"alternativePhoneStatus",
+    "title":"Elder Moving"
+  },
+  {
+    "id": "tool",  
+    "errorId": "secondaryPhoneNumberErrorMessage",
+    "statusId":"alternativePhoneStatus",
+    "title":"Tool not User Friendly"
+  }
+];
--- lyflynks_customer_app/src/modules/Transportation/TransportationDetails.tsx
+++ lyflynks_customer_app/src/modules/Transportation/TransportationDetails.tsx
@@ -5,17 +5,16 @@ import { Thumbnail, CheckBox, Title, Subtitle } from 'native-base';
 import {
     colorSwatch,
     deviceWidth
-  } from 'styles/Theme';
+} from 'styles/Theme';
 import { Actions, InfoCard } from './components';
 import { EditIcon, CalendarIcon, ClockIcon, ActivityLogIcon } from 'components/icons';
 import { CustomCard } from './components/Cards';
 import CheckInAPIs from './api';
 import { connect } from 'react-redux';
 import { cancelCheckIn } from './action';
-
 const Member = ({ name }) => (
-    <View style={{ padding : 16, alignItems: 'center' }}>
-        <Thumbnail source={ require('images/elder-01.png') } />        
+    <View style={{ padding: 16, alignItems: 'center' }}>
+        <Thumbnail source={require('images/elder-01.png')} />
         <Title style={{ color: colorSwatch.codGray }}>{name}</Title>
         <Subtitle style={{ color: colorSwatch.dustyGray }}>Elder</Subtitle>
     </View>
@@ -23,14 +22,14 @@ const Member = ({ name }) => (
 
 const RequestBy = ({ name }) => (
     <CustomCard>
-        <View style={{flex: 1, alignItems: 'flex-start'}}>
+        <View style={{ flex: 1, alignItems: 'flex-start' }}>
             <Subtitle style={{ color: colorSwatch.dustyGray }}>REQUESTED BY</Subtitle>
         </View>
-        <View style={{ flex : 2, flexDirection: 'row', paddingTop: 8}}>
-            <View style={{ flex:2, alignItems: 'flex-start', paddingLeft: 2, justifyContent: 'center', borderRightWidth: 2, borderColor: colorSwatch.bombayGray}}>
-                <Subtitle style={{ color: colorSwatch.codGray }}>{ name }</Subtitle>
+        <View style={{ flex: 2, flexDirection: 'row', paddingTop: 8 }}>
+            <View style={{ flex: 2, alignItems: 'flex-start', paddingLeft: 2, justifyContent: 'center', borderRightWidth: 2, borderColor: colorSwatch.bombayGray }}>
+                <Subtitle style={{ color: colorSwatch.codGray }}>{name}</Subtitle>
             </View>
-            <View style={{ flex:3, justifyContent: 'center', alignItems: 'flex-start', paddingLeft: 16}}>
+            <View style={{ flex: 3, justifyContent: 'center', alignItems: 'flex-start', paddingLeft: 16 }}>
                 <Subtitle style={{ color: colorSwatch.codGray }}>August 2, 2018</Subtitle>
             </View>
         </View>
@@ -39,12 +38,12 @@ const RequestBy = ({ name }) => (
 
 const AnyMemberCanComplete = ({ checked }) => (
     <View style={{ flexDirection: 'row' }}>
-        <View style={{flex: 1, alignItems: 'flex-start', paddingLeft: 8}}>
+        <View style={{ flex: 1, alignItems: 'flex-start', paddingLeft: 8 }}>
             <Subtitle style={{ color: colorSwatch.codGray }}>CHECK IN</Subtitle>
         </View>
-        <View style={{flex: 2, flexDirection: 'row', alignItems: 'flex-end'}}>
+        <View style={{ flex: 2, flexDirection: 'row', alignItems: 'flex-end' }}>
             <Text>Any Member Can Complete</Text>
-            <CheckBox checked={checked} color={colorSwatch.caribbeanGreen}/>
+            <CheckBox checked={checked} color={colorSwatch.caribbeanGreen} />
         </View>
     </View>
 );
@@ -90,8 +89,8 @@ class TransportationDetails extends React.Component {
 
     handleOnCancel = () => {
         Alert.alert('Cancel', 'Are you sure?', [
-            {text: 'Cancel', onPress: ()=> {  }},
-            {text: 'Ok', onPress: ()=> {  this.cancelCheckIn() }}
+            { text: 'Cancel', onPress: () => { } },
+            { text: 'Ok', onPress: () => { this.cancelCheckIn() } }
         ])
     }
 
@@ -102,10 +101,10 @@ class TransportationDetails extends React.Component {
                 navigation={this.props.navigation}
                 title="Transportation Details"
                 back={true}
-                >
-                <Member name={data.elder_name}/>
+            >
+                <Member name={data.elder_name} />
                 <View style={{ marginTop: 8, marginBottom: 8, alignItems: 'center' }}>
-                    <ActivityLogIcon color={colorSwatch.bostonBlue} style={{}}  />
+                    <ActivityLogIcon color={colorSwatch.bostonBlue} style={{}} />
                     <Subtitle style={{ color: colorSwatch.bostonBlue }}>
                         CHECK IN COMPLETED
                     </Subtitle>
@@ -117,18 +116,18 @@ class TransportationDetails extends React.Component {
                 <InfoCard
                     field={3}
                     editable={false}
-                    icon={<CalendarIcon style={{}} />} 
-                    text={'August 8, 2018'} 
+                    icon={<CalendarIcon style={{}} />}
+                    text={'August 8, 2018'}
                 />
                 <InfoCard
                     field={4}
                     editable={false}
-                    icon={<ClockIcon style={{}} />} 
-                    text={'05:40 PM'} 
+                    icon={<ClockIcon style={{}} />}
+                    text={'05:40 PM'}
                 />
-                <RequestBy name={data.requested_member_name}/>
-                <Actions 
-                    onSubmit={this.handleOnSubmit} 
+                <RequestBy name={data.requested_member_name} />
+                <Actions
+                    onSubmit={this.handleOnSubmit}
                     submitText="Update"
                     onCancel={this.handleOnCancel}
                 />
